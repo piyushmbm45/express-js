b7dcf30eca775471ead1454a8fc042e2cda25997
Piyush Jain <piyushmbm45@gmail.com>
2021-06-22 17:49:13 +0530

    learning from youtube coder's gyan

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path'); 
+const app = express();
+
+app.get('/',(req,res)=>{
+    // res.send('Hello Express World');
+    // returning html-inline
+    // res.send('<h1>Its Learning Time</h1>')
+    //returning html file
+    res.sendFile(path.resolve(__dirname)+'/index.html')
+})
+// creating new route
+app.get('/about',(req,res)=>{
+    res.sendFile(path.resolve(__dirname)+'/about.html')
+})
+
+
+const port = process.env.PORT || 4000;
+
+app.listen(port,()=>{
+    console.log(`Listening on Port ${port}`);
+})
