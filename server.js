52f30b20e77ec441f4b6ec7833ec31dfb4c77f4c
Piyush Jain <piyushmbm45@gmail.com>
2021-06-22 18:18:53 +0530

    moving forward to file download

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const app = express();
 // app.use(express.static('public')); for express middleware
 
 
-// app.get('/',(req,res)=>{
-//     // res.send('Hello Express World');
-//     // returning html-inline
-//     // res.send('<h1>Its Learning Time</h1>')
-//     //returning html file
-//     res.sendFile(path.resolve(__dirname)+'/index.html')
-// })
-// // creating new route
-// app.get('/about',(req,res)=>{
-//     res.sendFile(path.resolve(__dirname)+'/about.html')
-// })
+app.get('/',(req,res)=>{
+    // res.send('Hello Express World');
+    // returning html-inline
+    // res.send('<h1>Its Learning Time</h1>')
+    //returning html file
+    res.sendFile(path.resolve(__dirname)+'/index.html')
+})
+// creating new route
+app.get('/about',(req,res)=>{
+    res.sendFile(path.resolve(__dirname)+'/about.html')
+})
 
 
 const port = process.env.PORT || 4000;
