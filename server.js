98599f7de1444338089f42b894d75a8785bd86b3
Piyush Jain <piyushmbm45@gmail.com>
2021-06-22 18:16:10 +0530

    static middleware

--- server.js
+++ server.js
@@ -2,17 +2,20 @@ const express = require('express');
 const path = require('path'); 
 const app = express();
 
-app.get('/',(req,res)=>{
-    // res.send('Hello Express World');
-    // returning html-inline
-    // res.send('<h1>Its Learning Time</h1>')
-    //returning html file
-    res.sendFile(path.resolve(__dirname)+'/index.html')
-})
-// creating new route
-app.get('/about',(req,res)=>{
-    res.sendFile(path.resolve(__dirname)+'/about.html')
-})
+// app.use(express.static('public')); for express middleware
+
+
+// app.get('/',(req,res)=>{
+//     // res.send('Hello Express World');
+//     // returning html-inline
+//     // res.send('<h1>Its Learning Time</h1>')
+//     //returning html file
+//     res.sendFile(path.resolve(__dirname)+'/index.html')
+// })
+// // creating new route
+// app.get('/about',(req,res)=>{
+//     res.sendFile(path.resolve(__dirname)+'/about.html')
+// })
 
 
 const port = process.env.PORT || 4000;
